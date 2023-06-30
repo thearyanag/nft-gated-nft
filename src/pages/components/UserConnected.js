8fcccb17fa1936c27ec722cfbe63e902e5f62788
aryan <agarwalaryan139@gmail.com>
2023-06-30 15:44:31 +0530

    build fix

--- src/pages/components/UserConnected.js
+++ src/pages/components/UserConnected.js
@@ -7,6 +7,11 @@ import Image from 'next/image';
 
 export const UserConnected = () => {
   const { data: session } = useSession();
+
+  if (!session) {
+    return null;
+  }
+
   return (
     <div>
         <Dropdown>
